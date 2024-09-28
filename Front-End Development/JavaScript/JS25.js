72435b8d1566b6c8f5b3ef21dd9b371a38fc6dad
Md. Mehraj Hossain Akash <mehrajakash81@gmail.com>
2024-09-28 22:08:55 +0600

    Update JS25

--- Front-End Development/JavaScript/JS25.js
+++ Front-End Development/JavaScript/JS25.js
@@ -1,7 +1,23 @@
 
-document.write("haerljf ")
+document.write("haerljf ");
+
+(function sum(num1,num2){
+    console.log(num1+num2);
+})(15,21);
 
 (function display(message){
     console.log(message);
     document.write(message);
 })("HelloBangladesh");
+
+
+// Function Expressions
+const display2 = function displayMessage(){
+    console.log("This is function Expression.");
+};
+display2(); //calling display2 function.
+
+var display3 = function displaythree(mes){
+    console.log(mes);
+};
+display3("This is another display");
