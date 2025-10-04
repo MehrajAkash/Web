c284c767d2336683a278e9f431ef088dc4d0320a
Md. Mehraj Hossain Akash <mehrajakash81@gmail.com>
2025-10-05 00:11:43 +0600

    js

---
+++ Projects/Weather App/script.js
@@ -0,0 +1,20 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "";
+
+weatherForm.addEventListener("submit", event => {
+
+});
+
+async function getWeatherData(city) {
+
+}
+
+function displayWeatherInfo(data) {
+
+}
+
+function getWeatherEmoji(weather) {
+
+}
